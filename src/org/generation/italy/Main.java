d0e8246db30c4f48f7d7ea752f6da696072c691e
Andrea Cuccuru <cuccuruandrea16@gmail.com>
2023-11-15 12:19:37 +0100

    primo commit

---
+++ src/org/generation/italy/Main.java
@@ -0,0 +1,26 @@
+package org.generation.italy;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner sc=new Scanner(System.in);
+		
+		Float[] carte={1f,2f,3f,4f,5f,6f,7f,0.5f,0.5f,0.5f,
+					   1f,2f,3f,4f,5f,6f,7f,0.5f,0.5f,0.5f,
+					   1f,2f,3f,4f,5f,6f,7f,0.5f,0.5f,0.5f,
+					   1f,2f,3f,4f,5f,6f,7f,0.5f,0.5f,0.5f,};
+		
+	
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
