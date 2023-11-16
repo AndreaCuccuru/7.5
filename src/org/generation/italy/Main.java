3e29fe4e666f04029608d30bcf917936caef2070
Andrea Cuccuru <cuccuruandrea16@gmail.com>
2023-11-16 11:00:07 +0100

    finito

--- src/org/generation/italy/Main.java
+++ src/org/generation/italy/Main.java
@@ -1,5 +1,6 @@
 package org.generation.italy;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
@@ -8,19 +9,46 @@ public class Main {
 		
 		Scanner sc=new Scanner(System.in);
 		
-		Float[] carte={1f,2f,3f,4f,5f,6f,7f,0.5f,0.5f,0.5f,
+		Float[] elencoCarte={1f,2f,3f,4f,5f,6f,7f,0.5f,0.5f,0.5f,
 					   1f,2f,3f,4f,5f,6f,7f,0.5f,0.5f,0.5f,
 					   1f,2f,3f,4f,5f,6f,7f,0.5f,0.5f,0.5f,
-					   1f,2f,3f,4f,5f,6f,7f,0.5f,0.5f,0.5f,};
+					   1f,2f,3f,4f,5f,6f,7f,0.5f,0.5f,0.5f,};	
+		int carta = 0;
+		Float contatore=0f,tot=0f;
+		Random R=new Random();
+		String risposta,risposta2;
+		do
+		{
+			do
+			{		
+				carta=R.nextInt(elencoCarte.length);
+				System.out.println("la carta pescata: "+ elencoCarte[carta]);
+				contatore=elencoCarte[carta];
+				tot=tot+contatore;
+				System.out.println("stai a: " +tot);
+				if (tot>7.5f)
+				{
+					System.out.println("hai sballato");
+					break;
+				}
+				if (tot==7.5f)
+				{
+					System.out.println("grande!"); 
+					break;
+				}
+				if (tot<7.5f)
+					System.out.println("vuoi pescare un'altra carta?"); 
+					risposta=sc.nextLine();		
+					
+				} while (risposta.equals("si"));
 		
-	
+		System.out.println("vuoi giocare ancora?");
+		risposta2=sc.nextLine();	
+		tot=0f;
 		
+		} while (risposta2.equals("si"));	
 		
-		
-		
-		
-		
-
+sc.close();
 	}
 
 }
